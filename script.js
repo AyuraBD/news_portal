763b45aaeb9b54f207d9f2237bb9d84999825624
Mohammad Ayub <5043ayub@gmail.com>
2022-09-03 19:11:49 +0600

    Click to show details of posts added

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const getMenu = () =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
     .then(res => res.json())
-    .then(data => deisplayMenu(data.data.news_category));
+    .then(data => displayMenu(data.data.news_category));
 }
-const deisplayMenu = posts =>{
-    console.log(posts);
+const displayMenu = posts =>{
+    // console.log(posts);
     const menuContainer = document.getElementById('menu-container');
     posts.forEach(post =>{
         const menuDiv = document.createElement('div');
@@ -17,6 +17,7 @@ const deisplayMenu = posts =>{
         menuContainer.appendChild(menuDiv);
     })
 }
+
 // News Post
 const newsPost = (category_id) =>{
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
@@ -27,17 +28,82 @@ const newsPost = (category_id) =>{
 const displayPost = posts =>{
   console.log(posts);
   const displayPostCategory = document.getElementById('display-post');
+        displayPostCategory.innerHTML = ``;
+    posts.forEach(post =>{
+    const newDiv = document.createElement('div');
+      newDiv.classList.add('col');
+      newDiv.innerHTML = `
+      <div class="row">
+        <div class="col-md-4">
+          <img class="img-fluid" src="${post.image_url}" class="card-img-top" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body mb-4">
+            <h5 class="card-title mb-2">${post.title}</h5>
+            <p class="card-text mb-4">${post.details.slice(0, 250) +' ...'}</p>
+          </div>
+          <div class=" bottom-cnt">
+            <div>
+              <div class="author">
+                <img class="img-fluid" src="${post.author.img}" alt="">
+                <h6>${post.author.name ? post.author.name : 'no data found'}</h6>
+              </div>
+            </div>
+            <div>
+              <p>${post.rating.number}</p>
+            </div>
+            <div>
+              <a onclick="newsDetails(${post})" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="cursor:pointer;">Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      `;
+      displayPostCategory.appendChild(newDiv);
+    })      
+};
+getMenu();
+
+// Click to show details of posts
+const newsDetails = (category_id) =>{
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayDetails(data.data));
+}
+const displayDetails = posts =>{
+  console.log(posts);
+  const staticBackdropLabel = document.getElementById('staticBackdropLabel');
+  // staticBackdropLabel.innerText = 
+}
+newsDetails();
+
+
+
+
+
+
+
+
+// Default Posts
+const newsPosts = () =>{
+  const url = `https://openapi.programming-hero.com/api/news/category/08`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayPosts(data.data));
+}
+const displayPosts = posts =>{
+  // console.log(posts);
+  const displayPostCategory = document.getElementById('display-post');
 
         displayPostCategory.innerHTML = ``;
     posts.forEach(post =>{
           const newDiv = document.createElement('div');
 
-          const size = Object.keys(newDiv).length;
+          const size = Object.keys(newDiv).length;          
+          const countDiv = document.getElementById('count');        
           
-          const countDiv = document.getElementById('count');
-          
-          
-          countDiv.innerHTML = size;
+          countDiv.innerText = size;
 
       newDiv.classList.add('col');
       newDiv.innerHTML = `
@@ -62,7 +128,7 @@ const displayPost = posts =>{
               <p>${post.rating.number}</p>
             </div>
             <div>
-              <a>Read More</a>
+              <a  onclick="newsDetails()" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style="cursor:pointer;">Read More</a>
             </div>
           </div>
         </div>
@@ -71,5 +137,4 @@ const displayPost = posts =>{
       displayPostCategory.appendChild(newDiv);
         })      
 };
-
-getMenu();
+newsPosts();
